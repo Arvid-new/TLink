5fadbbda132b1d6d6f2fbcf499f0bf319a78e791
Vincent Lore <vincent_lore@hotmail.com>
2015-03-17 13:24:08 -0700

    Customer queries for insert, delete, display

--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,66 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Customer {
 
-
+	Connection con = OracleConnection.getInstance().getConnection();
+	
+	public Customer () {}
+	
+	public void insertCustomer(int cid, String name) {
+		try {
+			PreparedStatement stmt = con.prepareStatement("INSERT INTO customer VALUES (?, ?)");
+			stmt.setInt(1, cid);
+			stmt.setString(2, name);
+			stmt.executeUpdate();
+			con.commit();
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				//TODO
+			}
+		}
+	}
+	
+	public void deleteCustomer(int cid) {
+		try {
+			Statement stmt = con.createStatement();
+			stmt.executeQuery("DELETE FROM customer WHERE cid = " + cid);
+			con.commit();
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				//TODO
+			}
+		}
+	}
+	
+	public void displayCustomers() {
+		int cid;
+		String name;
+		
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM customer");
+			
+			while (rs.next()) {
+				cid = rs.getInt("cid");
+				name = rs.getString("name");
+			}
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			//TODO
+		}
+	}
 }
