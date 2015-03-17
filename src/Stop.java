7da3bcae847b00f591bf614c74d2c4cfefac9910
Vincent Lore <vincent_lore@hotmail.com>
2015-03-17 13:26:15 -0700

    Stop queries for insert, delete, display

--- src/Stop.java
+++ src/Stop.java
@@ -1,4 +1,69 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Stop {
 	
+	Connection con = OracleConnection.getInstance().getConnection();
+	
+	public Stop() {}
+	
+	public void insertStop(int stopNum, String stopName, String location) {
+		try {
+			PreparedStatement stmt = con.prepareStatement("INSERT INTO stop VALUES (?, ?, ?)");
+			stmt.setInt(1, stopNum);
+			stmt.setString(2, stopName);
+			stmt.setString(3, location);
+			stmt.executeUpdate();
+			con.commit();
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				//TODO
+			}
+		}
+	}
+	
+	public void deleteDriver(int stopNum) {
+		try {
+			Statement stmt = con.createStatement();
+			stmt.executeQuery("DELETE FROM stop WHERE stopNum = " + stopNum);
+			con.commit();
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				//TODO
+			}
+		}
+	}
+	
+	public void displayDrivers() {
+		int stopNum;
+		String stopName;
+		String location;
+		
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM stop");
+			
+			while (rs.next()) {
+				stopNum = rs.getInt("empId");
+				stopName = rs.getString("name");
+				location = rs.getString("address");
+			}
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			//TODO
+		}
+	}
 }
