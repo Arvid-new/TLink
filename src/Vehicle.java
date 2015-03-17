0363816289b05d3b611ae27428ce43cce65e2d5e
Vincent Lore <vincent_lore@hotmail.com>
2015-03-17 13:29:42 -0700

    Vehicle queries for insert, delete, display

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,69 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Vehicle {
-
+	
+	Connection con = OracleConnection.getInstance().getConnection();
+	
+	public Vehicle() {}
+	
+	public void insertVehicle(int vehicleNum, int age, int capacity) {
+		try {
+			PreparedStatement stmt = con.prepareStatement("INSERT INTO vehicle VALUES (?, ?, ?)");
+			stmt.setInt(1, vehicleNum);
+			stmt.setInt(2, age);
+			stmt.setInt(3, capacity);
+			stmt.executeUpdate();
+			con.commit();
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				//TODO
+			}
+		}
+	}
+	
+	public void deleteVehicle(int vehicleNum) {
+		try {
+			Statement stmt = con.createStatement();
+			stmt.executeQuery("DELETE FROM vehicle WHERE vehicleNum = " + vehicleNum);
+			con.commit();
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				//TODO
+			}
+		}
+	}
+	
+	public void displayVehicles() {
+		int vehicleNum;
+		int age;
+		int capacity;
+		
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM vehicle");
+			
+			while (rs.next()) {
+				vehicleNum = rs.getInt("vehicleNum");
+				age = rs.getInt("age");
+				capacity = rs.getInt("capacity");
+			}
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			//TODO
+		}
+	}
 }
