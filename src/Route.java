9488517aa21f39433535353e8d31fc205b7b9b8f
vinlore <vincent_lore@hotmail.com>
2015-03-19 16:24:02 -0700

    Insert, delete display queries for route

--- src/Route.java
+++ src/Route.java
@@ -1,6 +1,62 @@
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class Route {
-
+	Connection con = OracleConnection.getInstance().getConnection();
+	
+	public Route() {}
+	
+	public void insertRoute(int routeNum, String rname, Date start, Date end) {
+		try {
+			PreparedStatement stmt = con.prepareStatement("INSERT INTO route VALUES (?, ?, ?, ?)");
+			stmt.setInt(1, routeNum);
+			stmt.setString(2, rname);
+			stmt.setDate(3, start);
+			stmt.setDate(4, end);
+			stmt.executeUpdate();
+			con.commit();
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				//TODO
+			}
+		}
+	}
+	
+	public void deleteRoute(int routeNum) {
+		try {
+			Statement stmt = con.createStatement();
+			stmt.executeQuery("DELETE FROM route WHERE routeNum = " + routeNum);
+			con.commit();
+			stmt.close();
+		}
+		catch (SQLException ex) {
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				//TODO
+			}
+		}
+	}
+	
+	public ResultSet displayRoutes() {
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM route");
+			stmt.close();
+			return rs;
+		}
+		catch (SQLException ex) {
+			//TODO
+			return null;
+		}
+	}
 }
