07ea6c21f6e5c37c583f768631c7f6b0b0821413
vinlore <vincent_lore@hotmail.com>
2015-03-22 01:50:59 -0700

    update balance works

--- src/Customer.java
+++ src/Customer.java
@@ -76,22 +76,14 @@ public class Customer {
 	}
 	
 	// adds to current balance
-	public ResultTableModel updateBalance(int cid, int add) {
+	public void updateBalance(int cid, int add) {
 		
 		try {
-			Statement stmt = con.createStatement();
-			String selectBalQueryStr = "SELECT balance FROM owns_pass WHERE cid =" + cid;
-			ResultSet initResult = stmt.executeQuery(selectBalQueryStr);
-			int initBal = initResult.getInt("balance");
-			int newBal = initBal + add;
-			String newBalStr = String.valueOf(newBal);
-			String updateStr = "UPDATE owns_pass SET balance = " + newBalStr;
-			stmt.executeUpdate(updateStr);
-			ResultSet rs = stmt.executeQuery(selectBalQueryStr);
-			ResultTableModel rtm = new ResultTableModel(rs);
-			rs.close();
-			con.close();
-			return rtm;
+			PreparedStatement ps = con.prepareStatement("UPDATE owns_pass SET balance = balance + ? WHERE cid =" + cid);
+			ps.setInt(1, add);
+			ps.executeUpdate();
+			con.commit();
+			ps.close();
 		}
 		catch (SQLException ex) {
 			try {
@@ -99,7 +91,6 @@ public class Customer {
 			} catch (SQLException e) {
 				// TODO
 			}
-			return null;
 		}
 	}
 	
--- src/TLinkFrame.java
+++ src/TLinkFrame.java
@@ -328,14 +328,15 @@ public class TLinkFrame extends JFrame {
 				}
 				Customer customer = new Customer();
 				customer.updateBalance(cid, amtToAdd);
-				ResultTableModel updateBalanceResults = customer.updateBalance(cid, amtToAdd);
+				customerTable.setModel(customer.displayBalance(cid));
+				/*ResultTableModel updateBalanceResults = customer.updateBalance(cid, amtToAdd);
 				if (updateBalanceResults.empty) {
 					JOptionPane.showMessageDialog(null, "No result found for that Customer ID");
 				}
 				else {
 					customerTable.removeAll();
 					customerTable.setModel(updateBalanceResults);
-				}
+				}*/
 			}
 		});
 		
