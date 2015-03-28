944684c78787813a48365c36818e1ef26a933363
vinlore <vincent_lore@hotmail.com>
2015-03-27 19:13:22 -0700

    Comments and message print out on exception

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -12,6 +12,7 @@ public class Vehicle {
 	
 	public Vehicle() {}
 	
+	// Inserts a tuple into vehicle table
 	public boolean insertVehicle(int vehicleNumber, int age, int capacity) {
 		try {
 			PreparedStatement stmt = con.prepareStatement("INSERT INTO vehicle VALUES (?, ?, ?)");
@@ -23,10 +24,12 @@ public class Vehicle {
 			return true;
 		}
 		catch (SQLException ex) {
+			System.out.println("Message: " + ex.getMessage());
 			return false;
 		}
 	}
 	
+	// Removes the tuple with the vehicleNumber from vehicle table
 	public boolean deleteVehicle(int vehicleNumber) {
 		try {
 			Statement stmt = con.createStatement();
@@ -35,10 +38,12 @@ public class Vehicle {
 			return (rows != 0) ? true : false;
 		}
 		catch (SQLException ex) {
+			System.out.println("Message: " + ex.getMessage());
 			return false;
 		}
 	}
 	
+	// Displays all vehicles from vehicle table
 	public ResultTableModel displayVehicles() {
 		try {
 			Statement stmt = con.createStatement();
@@ -48,11 +53,12 @@ public class Vehicle {
 			return rtm;
 		}
 		catch (SQLException ex) {
-			//TODO
+			System.out.println("Message: " + ex.getMessage());
 			return null;
 		}
 	}
 	
+	// Displays the vehicle with the given vehicleNumber from vehicle table
 	public ResultTableModel searchVehicles(int vehicleNumber) {
 		try {
 			Statement stmt = con.createStatement();
@@ -62,16 +68,17 @@ public class Vehicle {
 			return rtm;
 		}
 		catch (SQLException ex) {
-			//TODO
+			System.out.println("Message: " + ex.getMessage());
 			return null;
 		}
 	}	
-	
+
+	// The number of times a vehicle was driven in any given month
 	public ResultTableModel generateVehicleReport(int month) {
 		
 		try	{
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT vehicleNumber AS 'Vehicle Number', "
+			ResultSet rs = stmt.executeQuery("SELECT vehicleNumber, "
 					+ "COUNT(vehicleNumber) AS 'Number of times driven' "
 					+ "FROM driven_by WHERE MONTH(fromDate) = " + month + " GROUP BY vehicleNumber");
 			ResultTableModel rtm = new ResultTableModel(rs);
@@ -79,10 +86,8 @@ public class Vehicle {
 			return rtm;
 		}
 		catch (SQLException ex) {
-			//TODO 
+			System.out.println("Message: " + ex.getMessage());
 			return null;
 		}
 	}
-	
-	
 }
